c471b2ffa43cedd08a7b281d9889b2234bb6f5fe
Anders Simonsen <Anders Simonsen>
2016-08-21 17:40:46 +0200

    theGear spinner is ready to get wired up

--- assets/spinners.js
+++ assets/spinners.js
@@ -449,15 +449,13 @@ var theGear = (function(){
         width = null,
         height = null;
 
-    var arcRadiusA = 30,
+    var arcRadiusA = 20,
         arcRadiusB = arcRadiusA - (arcRadiusA / 5),
         number = 16,
-        speed = 0.03,
-        speedA = 0.04,
-        size = 'shrink',
+        speed = 0.06,
         lineWidth = arcRadiusB / 2,
-        innerOffset = 0.06,
-        outerOffset = 0.08;
+        innerOffset = 0.1,
+        outerOffset = 0.1;
 
     var particlesA = [];
     var particlesB = [];
@@ -484,60 +482,9 @@ var theGear = (function(){
     function render () {
         context.save();
 
-        for (var i = 0; i < number; i++) {
-            // // context.moveTo(particlesA[i].x = width / 2 + Math.cos(particlesA[i].angleStart) * arcRadiusA, particlesA[i].y = height / 2 + Math.sin(particlesA[i].angleStart) * arcRadiusA);
-            // // context.lineTo(particlesB[i].x = width / 2 + Math.cos(particlesB[i].angleStart) * arcRadiusB, particlesB[i].y = height / 2 + Math.sin(particlesB[i].angleStart) * arcRadiusB);
-            // // context.arc(particlesA[i].x = width / 2 + Math.cos(particlesA[i].angleStart) * arcRadiusA, particlesA[i].y = height / 2 + Math.sin(particlesA[i].angleStart) * arcRadiusA, 3, 0, Math.PI *2 , false);
-            // context.arc(particlesB[i].x = width / 2 + Math.cos(particlesB[i].angleStart) * arcRadiusB, particlesB[i].y = height / 2 + Math.sin(particlesB[i].angleStart) * arcRadiusB, 1, 0, Math.PI *2, false);
-            // // if (i < number -1) {
-            // //     // context.lineTo(particlesA[i + 1].x , particlesA[i + 1].y);
-            // // } else {
-            // //     // context.moveTo(particlesB[number-1].x , particlesB[number-1].y);
-            // //     // context.lineTo(particlesA[0].x, particlesA[0].y);
-            // // }
-            // context.strokeStyle = 'green';
-            // context.stroke();
-
-
-            // context.moveTo(particlesA[i].x = width / 2 + Math.cos(particlesA[i].angleStart) * arcRadiusA, particlesA[i].y = height / 2 + Math.sin(particlesA[i].angleStart) * arcRadiusA);
-
-
-            // context.beginPath();
-            // if (i % 2 !== 0) {
-            //     context.moveTo(particlesB[i].x = width / 2 + Math.cos(particlesB[i].angleStart) * arcRadiusB, particlesB[i].y = height / 2 + Math.sin(particlesB[i].angleStart) * arcRadiusB);
-            //     context.lineTo(particlesB[i -1].x = width / 2 + Math.cos(particlesB[i -1].angleStart) * arcRadiusB, particlesB[i -1].y = height / 2 + Math.sin(particlesB[i -1].angleStart) * arcRadiusB);
-            //
-            // }
-            // context.stroke();
-
-
-
-
-
-
-            // particlesA[i].angleStart += speed;
-            // particlesB[i].angleStart += speed;
-            //
-            // if (size === 'shrink') {
-            //     arcRadiusB -= speedA;
-            //     if (arcRadiusB <= arcRadiusA - 12 ) {
-            //         size = 'grow';
-            //     }
-            // } else if (size === 'grow') {
-            //     arcRadiusB += speedA;
-            //     //speedA -= 0.01 ;
-            //     if (arcRadiusB >= arcRadiusA) {
-            //         size = 'shrink';
-            //     }
-            // }
-        }
-
-
         for (var j = 0; j < number; j++) {
             context.beginPath();
-            // context.arc(particlesA[j].x = width / 2 + Math.cos(particlesA[j].angleStart) * arcRadiusA, particlesA[j].y = height / 2 + Math.sin(particlesA[j].angleStart) * arcRadiusA, 2, 0, Math.PI *2 , false);
-            context.strokeStyle = 'red';
-            context.fillStyle = 'red';
+            context.fillStyle = '#4b4b4b';
             if (j % 2 !== 0) {
                 context.moveTo(particlesB[j].x = width / 2 + Math.cos(particlesB[j].angleStart + innerOffset) * arcRadiusB, particlesB[j].y = height / 2 + Math.sin(particlesB[j].angleStart + innerOffset) * arcRadiusB);
                 context.lineTo(particlesB[j -1].x = width / 2 + Math.cos(particlesB[j -1].angleStart - innerOffset) * arcRadiusB, particlesB[j -1].y = height / 2 + Math.sin(particlesB[j -1].angleStart - innerOffset) * arcRadiusB);
@@ -545,23 +492,16 @@ var theGear = (function(){
                 context.lineTo(particlesA[j].x = width / 2 + Math.cos(particlesA[j].angleStart - outerOffset) * arcRadiusA, particlesA[j].y = height / 2 + Math.sin(particlesA[j].angleStart - outerOffset) * arcRadiusA);
                 context.lineTo(particlesB[j].x = width / 2 + Math.cos(particlesB[j].angleStart + innerOffset) * arcRadiusB, particlesB[j].y = height / 2 + Math.sin(particlesB[j].angleStart + innerOffset) * arcRadiusB);
             }
-
             context.fill();
-            context.stroke();
+            particlesA[j].angleStart += speed;
+            particlesB[j].angleStart += speed;
         }
 
         context.beginPath();
         context.arc(width / 2, height / 2, arcRadiusB - (lineWidth / 2), 0, Math.PI * 2, false);
+        context.strokeStyle = '#4b4b4b';
         context.lineWidth = lineWidth;
         context.stroke();
-
-
-
-
-        // context.beginPath();
-        // context.arc(width / 2, height / 2, arcRadiusA, 0, Math.PI * 2, false);
-        // context.strokeStyle = '#4b4b4b';
-        // context.stroke();
 
         context.restore();
     }
